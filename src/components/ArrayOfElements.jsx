0f03fb946f31c4b275c1de215826d48a1ec2fa7a
juliasavicke <juliasavicke@gmail.com>
2022-11-23 10:51:29 +0200

    map

--- src/components/ArrayOfElements.jsx
+++ src/components/ArrayOfElements.jsx
@@ -1,13 +1,23 @@
-const colors = ['red', 'green', 'blue', 'yellow'];
+const colors = [
+  { id: 1, value: 'red' },
+  { id: 2, value: 'green' },
+  { id: 3, covaluelor: 'blue' },
+  { id: 4, value: 'yellow' },
+];
 function ArrayOfElements() {
+  const h2Styles = {
+    fontSize: '3rem',
+    color: 'tomato',
+  };
   return (
     <div>
+      <h2 style={h2Styles}>Array of Elements</h2>
       <ul>
-        <li>Once</li>
-        <li>Once</li>
-        <li>Once</li>
-        <li>Once</li>
-        <li>Once</li>
+        {colors.map((cObj, i) => (
+          <li key={cObj.id} style={{ color: cObj.value }}>
+            {cObj.value}
+          </li>
+        ))}
       </ul>
     </div>
   );
